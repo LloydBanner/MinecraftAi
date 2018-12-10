ac6e3ab1f14d03030564f3f246918511780789f5
Lloyd Banner <lmb565@york.ac.uk>
2018-12-10 18:33:10 +0000

    ctypes base functionality

--- ai1.py
+++ ai1.py
@@ -1,5 +1,6 @@
 import time
 import ctypes
+from pynput.mouse import Controller
 from keyboard_inputs import PressKey, ReleaseKey
 
 
@@ -16,17 +17,36 @@ MOUSE_MIDDLEDOWN = 0x0020   # middle button down
 MOUSE_MIDDLEUP = 0x0040     # middle button up
 MOUSE_MOVE = 0x0001
 
+icon_x = 619
+icon_y = 800
+#Minecraft resume button location on bedrock, fullscreen 1080p
+resume_x = 574
+resume_y = 420
+
+#opens minecraft window and unpauses game if minecraft icon is fourth on hotbar
 def setup():
-    time.sleep(3)
-    #move_mouse(100, 0)
-    #mouse_press_left()
-    key_press(A)
-    time.sleep(10)
-    #mouse_release_left()
-    key_release(A)
+    set_mouse(icon_x, icon_y)
+    mouse_press_left()
+    mouse_release_left()
+    time.sleep(0.05)
+    set_mouse(resume_x, resume_y)
+    mouse_press_left()
+    mouse_release_left()
+
+def set_mouse(x, y):
+    width, height = get_mouse_position()
+    y_move = y - height
+    x_move = x - width
+    move_mouse(x_move, y_move)
 
 def move_mouse(x, y):
-    ctypes.windll.user32.mouse_event(MOUSE_MOVE, x, -y, 0, 0)
+    x = (x / 3.66)
+    y = (y / 3.11)
+    ctypes.windll.user32.mouse_event(MOUSE_MOVE, int(x), int(y), 0, 0)
+
+def get_mouse_position():
+    mouse = Controller()
+    return mouse.position
 
 def mouse_press_left():
     ctypes.windll.user32.mouse_event(MOUSE_LEFTDOWN, 0, 0, 0, 0)
